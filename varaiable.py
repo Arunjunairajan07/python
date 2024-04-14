d4b043f6812e594dc95c27ee626900cb35e7de70
Arunjunairajan07 <arunjunairajan07@gmail.com>
2024-04-14 20:07:32 +0530

    variable file

---
+++ varaiable.py
@@ -0,0 +1,25 @@
+#assign the varaiable
+
+Name="Anand"
+Days= 15
+Years= 2021
+print("Dear" + str( Name))
+print("you have" + str (Days) + "days of leave balance for this year" + str (Years))
+
+# get the user details
+
+User_name=input("what is your name :")
+User_Email=input("enter the emila_id :")
+User_ph=input("enter the phone number :")
+print("my name is :" + User_name)
+print("my email_id :"+ User_Email)
+print("my number is : 91"+ User_ph)
+
+#using in import math
+
+import math
+num=2
+n=0
+print(math.log2(num))
+print(math.cos(n))
+print(math.e)
